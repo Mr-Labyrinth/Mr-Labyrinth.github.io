f998537ee93b9de05a31e2bb897d712646653662
qizhizaizhe <qizhishijie@163.com>
2025-03-08 22:00:30 +0800

    Site updated: 2025-03-08 22:00:30

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/02/25/hello-world/","2025/02/25/第一篇博客，纪念/"];function toRandomPost(){
+var posts=["2025/02/25/第一篇博客，纪念/","2025/02/25/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
